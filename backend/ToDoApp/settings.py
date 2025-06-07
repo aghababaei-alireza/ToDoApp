d18a36c92520ebc635385eebb2f71e1369fe05a9
Alireza Aghababaei <aghababaei1375@gmail.com>
2025-06-07 23:12:04 +0330

    Update settings.py

--- backend/ToDoApp/settings.py
+++ backend/ToDoApp/settings.py
@@ -49,13 +49,13 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
     "mail_templated",
     "drf_recaptcha",
-    # "corsheaders",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    # "corsheaders.middleware.CorsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
